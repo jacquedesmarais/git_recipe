e21a6f08148562c8dec1ee7c2e16de6df518bc8f
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-21 18:35:56 -0500

    mixing glasses are the best for this

--- recipe.rb
+++ recipe.rb
@@ -2,6 +2,6 @@ Recipe :
 
 1. Add 1oz of gin to bottom of mixing glass
 2. Add 1oz of campari to bottom of mixing glass
-3. Add 1oz of sweet vermouth to bottom of shaker
-4. Shake and serve neat
+3. Add 1oz of sweet vermouth to bottom of mixing glass
+4. Stir and serve neat
 5. Serve with orange peel
