da411ad12a0e8ea04cdaf091738c6c461f4b055a
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-21 18:32:29 -0500

    added a recipe

---
+++ recipe.rb
@@ -0,0 +1,7 @@
+Recipe :
+
+1. Add 1oz of gin to bottom of shaker
+2. Add 1oz of campari to bottom of shaker
+3. Add 1oz of sweet vermouth to bottom of shaker
+4. Shake and serve neat
+5. Serve with orange peel
