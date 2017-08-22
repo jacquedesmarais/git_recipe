6e491f76f7ea1872e306ded0e7dca62e8226fdef
“jacquedesmaraisgit config --global user.email “jacquedes@gmail.com <jacquedes@gmail.com>
2017-08-21 19:28:33 -0500

    change amount of campari

--- recipe.rb
+++ recipe.rb
@@ -1,7 +1,7 @@
 Recipe :
 
 1. Add 1.5oz of gin to bottom of mixing glass
-2. Add 1oz of campari to bottom of mixing glass
+2. Add 1.5oz of campari to bottom of mixing glass
 3. Add 1oz of sweet vermouth to bottom of mixing glass
 4. Stir and serve neat
 5. Serve with orange peel
